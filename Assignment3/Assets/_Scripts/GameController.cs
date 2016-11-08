ea391b53548c28b3d266a86e82e7a96edcb4360e
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-11-07 22:20:33 -0500

    :star: Major additions: :star:

--- Assignment3/Assets/_Scripts/GameController.cs
+++ Assignment3/Assets/_Scripts/GameController.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//refernce to teh UI namespace
+using UnityEngine.UI;
+/**
+ * StudentID: 300833478
+ * Date: 07/11/2016
+ */ 
 public class GameController : MonoBehaviour {
+    // Private Instance Variables
+    private float _time;
+    private bool _isGameOver;
+
+    // PUBLIC INSTANCE VARIABLES
+    public AudioSource GamePlaySound;
+
+    public float TimeValue
+    {
+        get
+        {
+            return this._time;
+        }
+        set
+        {
+            this._time = value;
+                this.TimeLable.text = Mathf.Round(this._time).ToString();
+        }
+    }
+
+    [Header("UI Objects")]
+    public Text TimeLable;
 
 	// Use this for initialization
 	void Start () {
-	
+        this.TimeValue = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+            this.TimeValue += Time.deltaTime; ;
 	}
 }
---
+++ Assignment3/Assets/_Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+//refernce to teh UI namespace
+using UnityEngine.UI;
+/**
+ * StudentID: 300833478
+ * Date: 07/11/2016
+ */
+public class MainMenuScript : MonoBehaviour {
+    // PUBLIC INSTANCE VARIABLES
+    public AudioSource MainMenuSound;
+
+    [Header("UI Objects")]
+    public Button StartButton;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    // PUBLIC METHODS
+    public void Start_Game()
+    {
+        MainMenuSound.Stop();
+        Application.LoadLevel("Main");
+    }
+}

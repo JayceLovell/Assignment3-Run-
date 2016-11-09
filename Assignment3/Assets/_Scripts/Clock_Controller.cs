2cfc989479f8cdfdf35c5d3f4dc8cc6e2107c71b
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-11-08 22:22:29 -0500

    Added another type of model for Maze and item in game :clock1:

---
+++ Assignment3/Assets/_Scripts/Clock_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock_Controller : MonoBehaviour {
+
+    private GameObject _gameControllerObject;
+    public GameController _gameController;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //private methods
+    private void OnTriggerEnter(Collider other)
+    {
+        _gameController.TimeValue -= Random.Range(1, 5);
+        Destroy(gameObject);
+    }
+}

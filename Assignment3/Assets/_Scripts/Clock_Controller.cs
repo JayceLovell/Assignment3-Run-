e8acdf9dd18770eaca39722ec9c875bb3f0b664c
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-11-09 17:11:15 -0500

    Added more to maze and BIG BUG SPLAT :bug:

--- Assignment3/Assets/_Scripts/Clock_Controller.cs
+++ Assignment3/Assets/_Scripts/Clock_Controller.cs
@@ -5,17 +5,62 @@ public class Clock_Controller : MonoBehaviour {
 
     private GameObject _gameControllerObject;
     public GameController _gameController;
+    private bool _goingup;
+    private float _speed;
+    private Transform _transform;
+
+    // PUBLIC PROPERTIES+++++++++++++++++++++++++++++++++
+    public float Speed
+    {
+        get
+        {
+            return this._speed;
+        }
+        set
+        {
+            this._speed = value;
+        }
+    }
+
+
 
     // Use this for initialization
     void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        _goingup = true;
+        this._transform = this.GetComponent<Transform>();
+        this._speed = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _slowMovement();
+    }
     //private methods
+    private void _slowMovement()
+    {
+        Vector3 newPosition = this._transform.position;
+
+        if (this._transform.position.y > 3.1f)
+        {
+            _goingup = false;
+        }
+        else if (this._transform.position.y < 1.43f)
+        {
+            _goingup = true;
+        }
+
+        if (_goingup)
+        {
+            newPosition.y += this._speed;
+            this._transform.position = newPosition;
+        }
+        else
+        {
+            newPosition.y -= this._speed;
+            this._transform.position = newPosition;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         _gameController.TimeValue -= Random.Range(1, 5);
--- Assignment3/Assets/_Scripts/GameController.cs
+++ Assignment3/Assets/_Scripts/GameController.cs
@@ -11,6 +11,7 @@ public class GameController : MonoBehaviour {
     // Private Instance Variables
     private float _time;
     private bool _isGameOver;
+    private bool _isGamePause;
 
     // PUBLIC INSTANCE VARIABLES
     public AudioSource GamePlaySound;
@@ -30,14 +31,44 @@ public class GameController : MonoBehaviour {
 
     [Header("UI Objects")]
     public Text TimeLable;
+    public Text MenuTitle;
+    public Button BackToMainMenu;
+    public Button Resume;
 
 	// Use this for initialization
 	void Start () {
         this.TimeValue = 0.0f;
+        MenuTitle.gameObject.SetActive(false);
+        BackToMainMenu.gameObject.SetActive(false);
+        Resume.gameObject.SetActive(false);
+        _isGamePause = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!_isGamePause)
+        {
             this.TimeValue += Time.deltaTime; ;
-	}
+        }
+    }
+    // Private METHODS*******************************
+    private void _bringUpMenu()
+    {
+        _isGamePause = true;
+        MenuTitle.gameObject.SetActive(true);
+        BackToMainMenu.gameObject.SetActive(true);
+        Resume.gameObject.SetActive(true);
+    }
+    // Public METHODS*******************************
+    public void BackToMainScreen()
+    {
+
+    }
+    public void BringDownMenu()
+    {
+        _isGamePause = false;
+        MenuTitle.gameObject.SetActive(false);
+        BackToMainMenu.gameObject.SetActive(false);
+        Resume.gameObject.SetActive(false);
+    }
 }
